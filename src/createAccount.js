7fd86cf53acd464b89e94ff4b19a8e5b51d29d0c
B9018376 <b9018376@newcastle.ac.uk>
2020-12-09 15:50:38 +0000

    set up

--- src/createAccount.js
+++ src/createAccount.js
@@ -4,24 +4,28 @@ function createAccount() {
     return(
         <div>
             <h1>Create New Account</h1>
-            <label htmlFor="type">Type:</label>
+            <label for="type">Type:</label>
             <select id="type" name="type">
                 <option value="current">Current</option>
                 <option value="savings">Savings</option>
             </select>
             <br></br>
-            <label htmlFor="currency">Currency:</label>
-            <select id="currency" name="currency" onChange="generateGraph()">
+            <br></br>
+            <label for="currency">Currency:</label>
+            <select id="currency" name="currency">
                 <option value="£">£</option>
                 <option value="$">$</option>
                 <option value="€">€</option>
             </select>
             <br></br>
-            <label htmlFor="name">Name:</label>
+            <br></br>
+            <label for="name">Name:</label>
             <input type="text" id="name" name="name"></input>
             <br></br>
-            <label htmlFor="balance">Balance:</label>
+            <br></br>
+            <label for="balance">Balance:</label>
             <input type="number" step="0.01" id="balance" name="balance"></input>
+            <br></br>
             <br></br>
             <button type="button" onClick="Create()" formAction="">Submit</button>
         </div>
