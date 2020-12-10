06788835fdd6c281e3847b17e85a102f4c865b11
B9018376 <b9018376@newcastle.ac.uk>
2020-12-10 16:00:47 +0000

    Changes to Navigation

--- src/index.js
+++ src/index.js
@@ -1,30 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
-import {Home} from "./component/Home";
+import { BrowserRouter as Router, Route, Link} from "react-router-dom";
 import {SignUp} from "./component/SignUp";
-import {MoveMoney} from "./component/MoveMoney";
-import {CreateAccount} from "./component/CreateAccount";
 import {Login} from "./component/Login";
 
 
-
 class App extends React.Component{
+
     render(){
         return(
             <div>
                 <Router>
-                    <Switch>
-                        <Link to={"/signup"}>Sign Up</Link>
-                    </Switch>
-                    <br></br>
-                    <Switch>
-                        <Link to={"/login"}>Log In</Link>
-                    </Switch>
+                    <h1>Welcome to Stubank</h1>
+                    <Link to={"/signup"}><button>Sign Up</button></Link>
+                    <Link to={"/login"}><button>Log In</button></Link>
 
                     <Route path={"/signup"}> <SignUp /> </Route>
                     <Route path={"/login"}> <Login /> </Route>
                 </Router>
+
             </div>
         );
     }
