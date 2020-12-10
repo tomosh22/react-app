526933c817895631b8c4a9df7d1e0ba19dd1af9f
B9018376 <b9018376@newcastle.ac.uk>
2020-12-10 14:57:41 +0000

    Basic Navigation for login/signup

--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Home} from "./component/Home"
-import {SignUp} from "./component/SignUp"
-import {MoveMoney} from "./component/MoveMoney"
+import { BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
+import {Home} from "./component/Home";
+import {SignUp} from "./component/SignUp";
+import {MoveMoney} from "./component/MoveMoney";
 import {CreateAccount} from "./component/CreateAccount";
 import {Login} from "./component/Login";
+
 
 
 class App extends React.Component{
     render(){
         return(
             <div>
-                <Home/>
+                <Router>
+                    <Switch>
+                        <Link to={"/signup"}>Sign Up</Link>
+                    </Switch>
+                    <br></br>
+                    <Switch>
+                        <Link to={"/login"}>Log In</Link>
+                    </Switch>
+
+                    <Route path={"/signup"}> <SignUp /> </Route>
+                    <Route path={"/login"}> <Login /> </Route>
+                </Router>
             </div>
         );
     }
 }
 
-ReactDOM.render(<Login />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
