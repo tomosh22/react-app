1112e43f147a8ab423cee3a3a18c6cbd87e06e63
jevgenijguzikovskij <63454824+b9019367@users.noreply.github.com>
2020-12-12 20:33:02 +0200

    Restructured Home page and applied styles to it

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,21 +3,36 @@ import {CreateAccount} from "./CreateAccount";
 import {MoveMoney} from "./MoveMoney";
 import {Dashboard} from "./Dashboard";
 import {BrowserRouter as Router, Link, Route} from "react-router-dom";
-import logo from '../assets/IMG-STUBANK-LOGO.jpg'
+import logo from '../assets/IMG-STUBANK-LOGO.jpg';
+import favicon from '../assets/favicon.ico';
+import style from "../Style/styles.module.css";
 
 export class Home extends React.Component{
     render(){
         return(
             <div>
                 <Router>
-                    <Link to={"/dashboard"}><button>Dashboard</button></Link>
-                    <Link to={"/create_account"}><button>Create account</button></Link>
-                    <Link to={"/move_money"}><button>Move Money</button></Link>
+                    <header>
+                        <div className={style.stickyTop}>
+                            <img className={style.favicon} src={favicon} alt={"Transparent logo"}/>
+                            <a  href={"//localhost:3000"}>StuBank</a>
+                            <div className={style.navigationLinks}>
+                                <Link to={"/dashboard"}><button className={style.navLink}>Dashboard</button></Link>
+                                <Link to={"/create_account"}><button className={style.navLink}>Create account</button></Link>
+                                <Link to={"/move_money"}><button className={style.navLink}>Move Money</button></Link>
+                            </div>
+                        </div>
+                    </header>
                     <Route path={"/dashboard"}> <Dashboard /> </Route>
                     <Route path={"/create_account"}> <CreateAccount /> </Route>
                     <Route path={"/move_money"}> <MoveMoney /> </Route>
                 </Router>
-                <img src={logo} alt={"StuBank logo"} height={200} width={200}/>
+                <footer>
+                    <p>Contanct information: </p>
+                    <p>Sitemap: </p>
+                    <p>Related documents:</p>
+                    <img src={logo} alt={"StuBank logo"} height={200} width={200}/>
+                </footer>
             </div>
         );
     }
