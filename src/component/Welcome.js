4560fa1603bbc8ac04624056ace35dfc15e05786
jevgenijguzikovskij <63454824+b9019367@users.noreply.github.com>
2020-12-13 17:04:58 +0200

    Created welcome page, which will be default

---
+++ src/component/Welcome.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export class Welcome extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Welcome to StuBank</h1>
+           </div>
+        )
+    }
+}
