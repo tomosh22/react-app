ec258010ff21bd236d1dd26424df6129821c1838
jevgenijguzikovskij <63454824+b9019367@users.noreply.github.com>
2020-12-13 16:10:01 +0200

    Added comments and restructured css file

--- src/component/Home.js
+++ src/component/Home.js
@@ -5,35 +5,33 @@ import {Dashboard} from "./Dashboard";
 import {BrowserRouter as Router, Link, Route} from "react-router-dom";
 import logo from '../assets/IMG-STUBANK-LOGO.jpg';
 import favicon from '../assets/favicon.ico';
-import style from "../Style/styles.module.css";
+import style from "../Style/newStyles.module.css";
 
 export class Home extends React.Component{
     render(){
         return(
-            <div>
-                <Router>
-                    <header>
-                        <div className={style.stickyTop}>
-                            <img className={style.favicon} src={favicon} alt={"Transparent logo"}/>
-                            <a  href={"//localhost:3000"}>StuBank</a>
-                            <div className={style.navigationLinks}>
-                                <Link to={"/dashboard"}><button className={style.navLink}>Dashboard</button></Link>
-                                <Link to={"/create_account"}><button className={style.navLink}>Create account</button></Link>
-                                <Link to={"/move_money"}><button className={style.navLink}>Move Money</button></Link>
+            <Router>
+                <header>
+                    <div className={style.container}>
+                        <div className={style.header_inner}>
+                            <div className={style.logo}>
+                                <img className={style.favicon} src={favicon} alt={"Transparent logo"}/>
+                                <a  href={"//localhost:3000"}>StuBank</a>
                             </div>
+                            <nav>
+                                <Link to={"/dashboard"}><button className={style.nav_link}>Dashboard</button></Link>
+                                <Link to={"/create_account"}><button className={style.nav_link}>Create account</button></Link>
+                                <Link to={"/move_money"}><button className={style.nav_link}>Move Money</button></Link>
+                            </nav>
                         </div>
-                    </header>
+                    </div>
+                </header>
+                <div>
                     <Route path={"/dashboard"}> <Dashboard /> </Route>
                     <Route path={"/create_account"}> <CreateAccount /> </Route>
                     <Route path={"/move_money"}> <MoveMoney /> </Route>
-                </Router>
-                <footer>
-                    <p>Contanct information: </p>
-                    <p>Sitemap: </p>
-                    <p>Related documents:</p>
-                    <img src={logo} alt={"StuBank logo"} height={200} width={200}/>
-                </footer>
-            </div>
+                </div>
+            </Router>
         );
     }
 }
