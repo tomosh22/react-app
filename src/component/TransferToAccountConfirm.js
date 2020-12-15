0076876a4c4803f53c5c5c396210eb41212d8f12
B9018376 <b9018376@newcastle.ac.uk>
2020-12-15 10:22:36 +0000

    Review Details webpage for transferring to account

--- src/component/TransferToAccountConfirm.js
+++ src/component/TransferToAccountConfirm.js
@@ -1,16 +1,14 @@
 import React from "react";
 
-export class TransferToUserConfirm extends React.Component{
+export class TransferToAccountConfirm extends React.Component{
 
     render() {
         return (
             <div>
                 <h1>Review Details</h1>
-                <p>From: <b>{this.props.state.accFrom}</b></p>
-                <p>Payee: <b>{this.props.state.accName}</b></p>
-                <p>Payee Details: <b>{this.props.state.sortCode}   {this.props.state.accNumber}</b></p>
+                <p>From: <b>{this.props.state.accountFrom}</b></p>
+                <p>To: <b>{this.props.state.accountTo}</b></p>
                 <p>Amount: <b>{this.props.state.currency}{this.props.state.amount}</b></p>
-                <p>Reference: <b>{this.props.state.reference}</b></p>
                 <button type="submit">Authorise payment</button><br />
                 <button type="submit">Change details</button>
             </div>
