3e2737dd376380aea51e2875c4c74e0b3faa455d
Tom O'Shaughnessy <b9014614@ncl.ac.uk>
2020-12-26 16:36:20 +0000

    implemented login with no authentication

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {context} from "./Home";
 
 export class Login extends React.Component{
 
@@ -12,11 +13,23 @@ export class Login extends React.Component{
     handleChange = event => {
         // stores what user types in form in React
         this.setState ({[event.target.name] : event.target.value})
+        console.log("username " + this.state.username)
     }
 
-    handleSubmit = event => {
+    async handleSubmit(event,setUsername,setFirstName,setLastName,setLoggedIn,addAccoun){
         event.preventDefault();
-        this.validate();
+        //this.validate();
+        var username,salt,firstname,secondname,email
+        await fetch("http://localhost:3000/selectLoginUser/" + this.state.username, {
+            method: "GET"
+        }).then(response => response.json()).then(data => {username=data[0].Username;salt=data[0].Salt;firstname=data[0].FirstName;secondname=data[0].SecondName;email=data[0].Email})
+        console.log("salt: ",salt);
+        setUsername(username);
+        setFirstName(firstname);
+        setLastName(secondname);
+
+
+        this.props.history.push("/dashboard");
     }
 
     validate = event =>{
@@ -36,22 +49,26 @@ export class Login extends React.Component{
 
     render() {
         return (
-            <div>
-                <h1>Login</h1>
-                <form action="Login" id="LoginForm" method="post" onSubmit={this.handleSubmit}>
+            <context.Consumer>{({setUsername,setFirstName,setLastName,setLoggedIn,addAccount}) => (
+                <div>
+                    <h1>Login</h1>
+                    <form action="Login" id="LoginForm" method="post" onSubmit={e => this.handleSubmit(e,setUsername,setFirstName,setLastName,setLoggedIn,addAccount)}>
 
-                    <label htmlFor="username">Username: </label><br/>
-                    <input type="text" id="username" name="username" value={this.state.username} onChange={this.handleChange}/>
-                    <div style={{color:"red"}}>{this.state.usernameError}</div><br/>
+                        <label htmlFor="username">Username: </label><br/>
+                        <input type="text" id="username" name="username" value={this.state.username} onChange={this.handleChange}/>
+                        <div style={{color:"red"}}>{this.state.usernameError}</div><br/>
 
-                    <label htmlFor="password">Password: </label><br/>
-                    <input type="password" id="password" name="password" value={this.state.password}
-                           onChange={this.handleChange}/>
-                    <div style={{color:"red"}}>{this.state.passwordError}</div><br/>
+                        <label htmlFor="password">Password: </label><br/>
+                        <input type="password" id="password" name="password" value={this.state.password}
+                               onChange={this.handleChange}/>
+                        <div style={{color:"red"}}>{this.state.passwordError}</div><br/>
 
-                    <button type="submit">Submit</button>
-                </form>
-            </div>
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+            )}
+            </context.Consumer>
+
 
         );
     }
