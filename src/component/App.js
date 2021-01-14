1f073fb7c63e70961bc4485c5077c0b5e084d510
jevgenijguzikovskij <63454824+b9019367@users.noreply.github.com>
2021-01-14 23:04:33 +0200

    Applied new logic and moved main page from home -> App

--- src/component/App.js
+++ src/component/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {Component, useContext} from "react";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 
 import {Header} from "./Header";
@@ -11,6 +11,20 @@ import {MoveMoney} from "./MoveMoney";
 import {AdminLogin} from './Admin/AdminLogin'
 import {Home} from "./Home";
 import {AdminPage} from "./Admin/AdminPage";
+import {Logout} from "./Logout";
+
+export const context = React.createContext()
+
+export class Account{
+
+    constructor(name,type,balance,currency,accNumber){
+        this.name = name;
+        this.type = type;
+        this.balance = balance;
+        this.currency = currency;
+        this.accNumber = accNumber;
+    }
+}
 
 export class App extends Component{
     pageLook =()=>{
@@ -29,23 +43,61 @@ export class App extends Component{
         }
         else{
             return(
-                <div>
+                <context.Provider value={this.state}>
                     <Router>
                         <Header />
                         <div>
                             <Route path={"/"} exact> <Home /> </Route>
                             <Route path={"/dashboard"}> <Dashboard /> </Route>
-                            <Route path={"/login"}> <Login /> </Route>
+                            <Route path={"/login"} exact> <Login /> </Route>
                             <Route path={"/create_account"}> <CreateAccount /> </Route>
                             <Route path={"/register"}> <SignUp /> </Route>
                             <Route path={"/transfer"}> <TransferToAccount /> </Route>
                             <Route path={"/move_money"}> <MoveMoney /> </Route>
                             <Route path={"/signup"}> <SignUp /> </Route>
+                            <Route path={"/logout"}><Logout /></Route>
                         </div>
                     </Router>
-                </div>
+                </context.Provider>
             );
         }
+    }
+
+    state = {
+        resetState:() => this.resetState(),
+        //resetState:()=>console.log("dfsingdsoi"),
+        username:"usr",
+        setUsername:(usr) => this.setState({userName:usr}),
+        firstName:"bob",
+        setFirstName:(name) => this.setState({firstName:name}),
+        lastName:null,
+        setLastName:(name) => this.setState({lastName:name}),
+        loggedIn:false,
+        setLoggedIn:(bool) => this.setState({loggedIn:bool}),
+        accounts:[],
+        addAccount:(acc) => this.addAccount(acc),
+        removeAccount:(accNumber) => this.removeAccount(accNumber)
+    }
+    removeAccount(accNumber){
+        for(var x = 0; x<this.state.accounts.length;x++){
+            if(this.state.accounts[x].accNumber == accNumber){
+                var accountsCopy = JSON.parse(JSON.stringify(this.state.accounts))
+                accountsCopy.splice(x,1)
+                this.setState({accounts:accountsCopy})
+            }
+        }
+    }
+    addAccount(acc){
+        console.log("adding account",acc)
+        var accountsCopy = JSON.parse(JSON.stringify(this.state.accounts))
+        accountsCopy.push(acc)
+        this.setState({accounts:accountsCopy})
+    }
+    resetState(){
+        console.log("resetting")
+        this.setState({firstName:null})
+        this.setState({lastName:null})
+        this.setState({loggedIn:false})
     }
 
     render(){
