a07b8a07b73408f90e472c6268cca9cc3e4b4c5b
Tom O'Shaughnessy <b9014614@ncl.ac.uk>
2021-01-15 17:37:21 +0000

    added pie chart of account balances to dashboard

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -2,9 +2,23 @@ import React,{useContext} from "react";
 import {context} from "./Home"
 import {Link} from "react-router-dom";
 import style from "../assets/css/homePageStyle.module.css";
+import Chart from "react-google-charts"
+
+
+
 
 export class Dashboard extends React.Component{
+    getChartData(accounts){
+        const data = [["Account Name","Balance"]]
+        for(var x of accounts){
+            data.push([x.name,x.balance])
+        }
+        console.log(data)
+        return data
+    }
     render() {
+
+
         function Accounts(){
             const data = useContext(context);
             var accsArray = [];
@@ -14,14 +28,31 @@ export class Dashboard extends React.Component{
             return <div>{accsArray}</div>
         }
         return (
-            <div>
+
+            <context.Consumer>{({accounts}) => (
+
+                <div>
                 <h1>Dashboard</h1>
+                <Chart
+                    //width={'500px'}
+                    //height={'300px'}
+
+                    chartType="PieChart"
+                    loader={<div>Loading Chart</div>}
+                    data={this.getChartData(accounts)}
+                    options={{
+                        title: 'Account Balances',
+                        backgroundColor: {fill:"transparent"}
+                    }}
+                />
                 <Accounts/>
                 <div>
                     <Link to={"/create_account"}><button className={style.nav_link}>Create Account</button></Link>
                     <Link to={"/move_money"}><button className={style.nav_link}>Make Payment</button></Link>
                 </div>
             </div>
+            )}
+            </context.Consumer>
         )
     }
 }
