c9cb02c78785f9366e7e6d3211438d30f81fed93
Joel Tierney <j.d.tierney1@newcastle.ac.uk>
2021-01-27 13:56:30 +0000

    Code cleanup within Login.js

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,9 +1,10 @@
 import React from "react";
-import {Account, context,Transaction} from "./App";
+import {Account, context, Transaction} from "./App";
+
 const crypto = require("crypto");
 const twofactor = require("node-2fa")
 
-export class Login extends React.Component{
+export class Login extends React.Component {
 
     state = {
         username: "",
@@ -11,41 +12,61 @@ export class Login extends React.Component{
         usernameError: "",
         passwordError: "",
         secret: "",
-        secretError:""
+        secretError: ""
     };
     handleChange = event => {
         // stores what user types in form in React
-        this.setState ({[event.target.name] : event.target.value})
+        this.setState({[event.target.name]: event.target.value})
     }
-    async handleSubmit(event,setUsername,setFirstName,setLastName,setLoggedIn,addAccount,addTransaction){
+
+    async handleSubmit(event, setUsername, setFirstName, setLastName, setLoggedIn, addAccount, addTransaction) {
         event.preventDefault();
         await this.validate();
-        if (this.state.usernameError || this.state.passwordError || this.state.secretError){
+        if (this.state.usernameError || this.state.passwordError || this.state.secretError) {
             return null
         }
-        var username,hash,salt,firstname,secondname,email,secret
+        var username, hash, salt, firstname, secondname, email, secret
         await fetch("http://localhost:3000/selectHashAndSaltAndSecret/" + this.state.username, {
             method: "GET"
-        }).then(response => response.json()).then(data => {hash = data[0].Password;salt = data[0].Salt;secret = data[0].Secret})
+        }).then(response => response.json()).then(data => {
+            hash = data[0].Password;
+            salt = data[0].Salt;
+            secret = data[0].Secret
+        })
         console.log(hash);
         console.log(salt);
-        var hashCheck = crypto.createHmac("sha512",salt)
+        var hashCheck = crypto.createHmac("sha512", salt)
         hashCheck.update(this.state.password + salt)
         hashCheck = hashCheck.digest("hex")
 
         const newToken = twofactor.generateToken(secret)
         //if password is correct
         //if(true){
-        if(hash == hashCheck && twofactor.verifyToken(secret,this.state.secret)){
+        if (hash == hashCheck && twofactor.verifyToken(secret, this.state.secret)) {
             await fetch("http://localhost:3000/selectLoginUser/" + this.state.username, {
                 method: "GET"
-            }).then(response => response.json()).then(data => {username=data[0].Username;salt=data[0].Salt;firstname=data[0].FirstName;secondname=data[0].SecondName;email=data[0].Email})
+            }).then(response => response.json()).then(data => {
+                username = data[0].Username;
+                salt = data[0].Salt;
+                firstname = data[0].FirstName;
+                secondname = data[0].SecondName;
+                email = data[0].Email
+            })
             await fetch("http://localhost:3000/getUserAccounts/" + this.state.username, {
                 method: "GET"
-            }).then(response => response.json()).then(data => {for(var x of data){addAccount(new Account(x.Name,x.Type,x.Balance,x.Currency,x.AccNumber))}})
+            }).then(response => response.json()).then(data => {
+                for (var x of data) {
+                    addAccount(new Account(x.Name, x.Type, x.Balance, x.Currency, x.AccNumber))
+                }
+            })
             await fetch("http://localhost:3000/getUserTransactions/" + this.state.username, {
                 method: "GET"
-            }).then(response => response.json()).then(data => {console.log(data);for(var x of data){addTransaction(new Transaction(x.TransactionId,x.Amount,x.DateTime,x.NameTo,x.AccNumberTo,x.AccNumberFrom,x.Currency,x.Reference,x.Tag))}})
+            }).then(response => response.json()).then(data => {
+                console.log(data);
+                for (var x of data) {
+                    addTransaction(new Transaction(x.TransactionId, x.Amount, x.DateTime, x.NameTo, x.AccNumberTo, x.AccNumberFrom, x.Currency, x.Reference, x.Tag))
+                }
+            })
             //}).then(response => response.json()).then(data => console.log(data))
             setUsername(this.state.username);
             setFirstName(firstname);
@@ -54,51 +75,56 @@ export class Login extends React.Component{
             this.props.history.push("/dashboard");
         }
         //if password is incorrect
-        else{
+        else {
             alert("Incorrect login, please try again")
         }
 
     }
 
-    async validate(event){
+    async validate(event) {
         // validates the user's input
-       let usernameError = "";
-       let passwordError = "";
-       let secretError = "";
+        let usernameError = "";
+        let passwordError = "";
+        let secretError = "";
 
-        if (!this.state.username){
+        if (!this.state.username) {
             usernameError = "Username is required"
         }
-        if (!this.state.password){
-            passwordError= "Password is required"
+        if (!this.state.password) {
+            passwordError = "Password is required"
         }
-        if (!this.state.secret){
+        if (!this.state.secret) {
             secretError = "Google Authenticator code is required"
         }
 
-       this.setState({usernameError, passwordError, secretError})
+        this.setState({usernameError, passwordError, secretError})
     }
 
     render() {
         return (
-            <context.Consumer>{({setUsername,setFirstName,setLastName,setLoggedIn,addAccount,addTransaction}) => (
+            <context.Consumer>{({setUsername, setFirstName, setLastName, setLoggedIn, addAccount, addTransaction}) => (
                 <div>
                     <h1>Log in</h1>
-                    <form action="Login" id="LoginForm" method="post" onSubmit={e => this.handleSubmit(e,setUsername,setFirstName,setLastName,setLoggedIn,addAccount,addTransaction)}>
+                    <form action="Login" id="LoginForm" method="post"
+                          onSubmit={e => this.handleSubmit(e, setUsername, setFirstName, setLastName, setLoggedIn, addAccount, addTransaction)}>
 
                         <label htmlFor="username">Username: </label><br/>
-                        <input type="text" id="username" name="username" value={this.state.username} onChange={this.handleChange}/>
-                        <div style={{color:"red"}}>{this.state.usernameError}</div><br/>
+                        <input type="text" id="username" name="username" value={this.state.username}
+                               onChange={this.handleChange}/>
+                        <div style={{color: "red"}}>{this.state.usernameError}</div>
+                        <br/>
 
                         <label htmlFor="password">Password: </label><br/>
                         <input type="password" id="password" name="password" value={this.state.password}
                                onChange={this.handleChange}/>
-                        <div style={{color:"red"}}>{this.state.passwordError}</div><br/>
+                        <div style={{color: "red"}}>{this.state.passwordError}</div>
+                        <br/>
 
                         <label htmlFor="secret">Google Authenticator Code: </label><br/>
                         <input id="secret" name="secret" value={this.state.secret}
                                onChange={this.handleChange}/>
-                        <div style={{color:"red"}}>{this.state.secretError}</div><br/>
+                        <div style={{color: "red"}}>{this.state.secretError}</div>
+                        <br/>
                         <br/>
                         <button type="submit">Submit</button>
                     </form>
